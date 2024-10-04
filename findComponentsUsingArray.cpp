ad0827ea40493a04e2da133359439fee40d2b0e0
spowerrr <mpuspo2310304@bscse.uiu.ac.bd>
2024-10-05 00:35:18 +0600

    modified

--- findComponentsUsingArray.cpp
+++ findComponentsUsingArray.cpp
@@ -7,7 +7,8 @@ using namespace std;
 #define black 3
 #define inf INT_MAX
 
-class Graph {
+class Graph
+{
   int nVertices;
   int **matrix;
   bool directed;           // directed->true, undirected->false
@@ -17,15 +18,18 @@ class Graph {
   int componentCount;      // Total number of components
 
 public:
-  Graph(int n, bool dir) {
+  Graph(int n, bool dir)
+  {
     nVertices = n;
     directed = dir;
     matrix = new int *[n]; // stores the address of each row
     for (int i = 0; i < n; i++)
       matrix[i] = new int[n]; // each row contains n integers
     // initialize all cells with 0
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
         matrix[i][j] = 0;
       }
     }
@@ -39,27 +43,28 @@ public:
     componentCount = 0;
   }
 
-  ~Graph() {
-    // Free memory allocated for the matrix
-    for (int i = 0; i < nVertices; i++) {
-      delete[] matrix[i];
-    }
-    delete[] matrix;
+  // ~Graph() {
+  //   // Free memory allocated for the matrix
+  //   for (int i = 0; i < nVertices; i++) {
+  //     delete[] matrix[i];
+  //   }
+  //   delete[] matrix;
 
-    // Free memory allocated for the components
-    for (int i = 0; i < componentCount; i++) {
-      delete[] components[i];
-    }
-    delete[] components;
-    delete[] componentSizes;
+  //   // Free memory allocated for the components
+  //   for (int i = 0; i < componentCount; i++) {
+  //     delete[] components[i];
+  //   }
+  //   delete[] components;
+  //   delete[] componentSizes;
 
-    // Free other dynamically allocated memory
-    delete[] color;
-    delete[] d;
-    delete[] parent;
-  }
+  //   // Free other dynamically allocated memory
+  //   delete[] color;
+  //   delete[] d;
+  //   delete[] parent;
+  // }
 
-  void addEdge(int u, int v) {
+  void addEdge(int u, int v)
+  {
     if (u < 0 || v < 0 || u >= nVertices ||
         v >= nVertices) // check if edge is valid
       return;
@@ -69,33 +74,41 @@ public:
       matrix[v][u] = 1;
   }
 
-  bool isEdge(int u, int v) {
+  bool isEdge(int u, int v)
+  {
     if (u < 0 || v < 0 || u >= nVertices || v >= nVertices)
       return false;
     return matrix[u][v] == 1;
   }
 
-  void DFSUtil(int u, int *component, int &index) {
+  void DFSUtil(int u, int *component, int &index)
+  {
     color[u] = gray;
     component[index++] = u; // Add current vertex to the component
 
-    for (int v = 0; v < nVertices; v++) {
-      if (isEdge(u, v) && color[v] == white) {
+    for (int v = 0; v < nVertices; v++)
+    {
+      if (isEdge(u, v) && color[v] == white)
+      {
         DFSUtil(v, component, index);
       }
     }
     color[u] = black;
   }
 
-  void findComponents() {
-    for (int i = 0; i < nVertices; i++) {
+  void findComponents()
+  {
+    for (int i = 0; i < nVertices; i++)
+    {
       color[i] = white;
     }
 
     componentCount = 0;
 
-    for (int i = 0; i < nVertices; i++) {
-      if (color[i] == white) {
+    for (int i = 0; i < nVertices; i++)
+    {
+      if (color[i] == white)
+      {
         // Dynamically allocate memory for the current component
         int *component = new int[nVertices];
         int index = 0;
@@ -113,9 +126,11 @@ public:
     cout << "Number of components: " << componentCount << endl;
 
     // Print each component
-    for (int i = 0; i < componentCount; i++) {
+    for (int i = 0; i < componentCount; i++)
+    {
       cout << "Component " << i + 1 << ": ";
-      for (int j = 0; j < componentSizes[i]; j++) {
+      for (int j = 0; j < componentSizes[i]; j++)
+      {
         cout << components[i][j] << " ";
       }
       cout << endl;
@@ -123,7 +138,8 @@ public:
   }
 };
 
-int main() {
+int main()
+{
   Graph g(9, false); // 9 vertices and undirected graph
 
   g.addEdge(0, 1);
