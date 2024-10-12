6ea5d40704aab7f6e90f2ef012ebe9373e6391b1
spowerrr <mpuspo2310304@bscse.uiu.ac.bd>
2024-10-12 20:07:36 +0600

    modified

---
+++ codingNinja/leetcode/postOrder.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  vector<int> postorderTraversal(TreeNode *root) {
+    vector<int> result;
+    postorder(root, result);
+    return result;
+  }
+
+private:
+  void postorder(TreeNode *node, vector<int> &result) {
+    if (node != NULL) {
+      postorder(node->left, result);
+      postorder(node->right, result);
+      result.push_back(node->val);
+    }
+  }
+};
