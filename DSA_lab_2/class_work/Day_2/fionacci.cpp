ce8f5071d81221e498d289a634c5f95ab565e1b1
pacman-cli <puspopuspo520@gmail.com>
2025-10-06 23:37:21 +0600

    checking

--- DSA_lab_2/class_work/Day_2/fionacci.cpp
+++ DSA_lab_2/class_work/Day_2/fionacci.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 using namespace std;
 int fibonacci(int n) {
-  if (n == 0)
-    return 0;
+    if (n == 0) return 0;
 
-  if (n == 1)
-    return 1;
+    if (n == 1) return 1;
 
-  return fibonacci(n - 1) + fibonacci(n - 2);
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 int main() {
-  int n;
-  cin >> n;
-  cout << "Fionacci:" << fibonacci(n);
+    int n;
+    cin >> n;
+    cout << "Fionacci:" << fibonacci(n);
 }
